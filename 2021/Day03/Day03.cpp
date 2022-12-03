e8cf973239cf06d86453e7dadc2d8f85c30f9b93
James Chok <james.chok1000+github@gmail.com>
2022-12-03 10:37:21 +0000

    something changed idk

--- 2021/Day03/Day03.cpp
+++ 2021/Day03/Day03.cpp
@@ -3,6 +3,34 @@
 #include <vector>
 #include <fstream>
 
+std::vector<int> arange(int n){
+    std::vector<int> array(n, 0);
+    for (int i = 0; i < n; i++) {
+        array[i] = i;
+    }
+    return array;
+}
+
+
+std::vector<std::vector<bool>> parse_file(std::string filename) {
+    int N = size_of_input(filename);
+
+    std::vector<std::vector<bool>> parsed_file;
+    std::string val;
+    std::ifstream MyReadFile(filename);
+
+
+    while (getline(MyReadFile, val)) {
+        std::vector<bool> parsed_line(N, 0);
+        for (int i = 0; i < N; i++) {
+            parsed_line[i] = val[i] == '1';
+        }
+        parsed_file.push_back(parsed_line);
+    }
+    return parsed_file;
+}
+
+
 
 int size_of_input(std::string filename) {
     std::ifstream MyReadFile(filename);
@@ -28,7 +56,8 @@ int binary_to_decimal(std::vector<bool> binary_array){
     return val;
 }
 
-void part_one(std::string filename) {
+
+std::vector<bool> get_gamma_rate(std::string filename) {
     std::string val;
     std::ifstream MyReadFile(filename);
 
@@ -45,23 +74,61 @@ void part_one(std::string filename) {
     }
 
     std::vector<bool> gamma_rate(N, 0);
-    std::vector<bool> epsilon_rate(N, 0);
     
     for (int i = 0; i < N; i++){
         if (one_counter[i] > length - one_counter[i]) {
             gamma_rate[i] = 1;
-            epsilon_rate[i] = 0;
         } else {
             gamma_rate[i] = 0;
+        }
+    }
+
+    return gamma_rate;
+}
+
+
+std::vector<bool> get_epsilon_rate(std::string filename) {
+    std::string val;
+    std::ifstream MyReadFile(filename);
+
+    int N = size_of_input(filename);
+
+    std::vector<int> one_counter(N, 0);
+    int length = 0;
+
+    while (getline(MyReadFile, val)) {
+        for (int i = 0; i < N; i++) {
+            one_counter[i] += val[i] == '1';
+        }
+        length += 1;
+    }
+
+    std::vector<bool> epsilon_rate(N, 0);
+    
+    for (int i = 0; i < N; i++){
+        if (one_counter[i] > length - one_counter[i]) {
+            epsilon_rate[i] = 0;
+        } else {
             epsilon_rate[i] = 1;
         }
     }
+
+    return epsilon_rate;
+}
+
+
+void part_one(std::string filename) {
+    std::vector<bool> gamma_rate = get_gamma_rate(filename);
+    std::vector<bool> epsilon_rate = get_epsilon_rate(filename);
 
     std::cout << binary_to_decimal(gamma_rate) * binary_to_decimal(epsilon_rate) << "\n";
 }
 
 
 void part_two(std::string filename) {
+    std::vector<bool> gamma_rate = get_gamma_rate(filename);
+
+    std::vector<int> working_index = arange(10);
 
 }
 
